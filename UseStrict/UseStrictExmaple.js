40abc8fc40626ea4bb564b78c1f37754ea0b2760
senthil <thangarasu.senthil05@gmail.com>
2025-12-09 22:50:03 +0530

    test

---
+++ UseStrict/UseStrictExmaple.js
@@ -0,0 +1,27 @@
+
+
+let number=10;
+console.log(number)
+
+// let obj={name1:"playwright", name1:"playwright"};
+// console.log(obj.name1)
+
+// let a=20;
+// delete a;
+
+
+function sum(a,a)
+{
+    console.log("sum method")
+}
+sum(10,10)
+
+function add()
+{
+    "use strict"
+    x=10
+    y=20
+    console.log(x+y)
+}
+
+add()
---
+++ UseStrict/notes.md
@@ -0,0 +1,12 @@
+
+
+what is "use strict":
+====================
+
+use strict  is a special mode in javascript 
+
+"use strict" used in 2 ways:
+============================
+
+1. at the top file
+2. inside method
